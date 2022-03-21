f9ce96090be86fbf8a6ebb1fc64fd488d62fbd88
fahim-morshed-1102 <50029279+fahim-morshed-1102@users.noreply.github.com>
2022-03-21 16:03:20 +0600

    idk what I'm doing with my life

--- src/Driver.java
+++ src/Driver.java
@@ -6,18 +6,13 @@ import java.io.IOException;
 
 public class Driver {
     public static void main(String[] args) throws IOException {
-//        BufferedImage ogimg = ImageIO.read(new File("img.jpg"));
-//        ogimg = resizeImage(ogimg, 300);
-//        File outputFile = new File("outputFile.png");
-//        ImageIO.write(ogimg, "png", outputFile);
-//
-//        for(int i=1;i<11;i++){
+/*        for(int i=1;i<11;i++){
 //            BufferedImage img = ImageIO.read(new File("village\\"+i+".jpg"));
 //            img = resizeImage(img, 500,500);
 //            File output = new File("villageTraining\\"+i+".png");
 //            ImageIO.write(img, "png", output);
 //        }
-
+*/
         int[][] cityRGB = new int[10][500*500];
         int[][] villageRGB = new int[10][500*500];
 
@@ -29,50 +24,85 @@ public class Driver {
         }
         int cityCorrect = 0;
         int villageCorrect = 0;
+
+
         //city testing
         for(int i=0; i<10;i++){
-            double averageCity = 0;
-            double averageVillage = 0;
+            double[] distances = new double[18];
+            int[] type = new int[18];
 
-            for (int j =0; j<10; j++){
+            int count = 0;
+            for (int j =0; j<10; j++)
                 if(i!=j){
-                    averageCity += distance(cityRGB[j], cityRGB[i] );
-                    averageVillage += distance(villageRGB[j], cityRGB[i] );
+                    distances[count] = distance(cityRGB[j], cityRGB[i] );
+                    type[count] = 0;
+                    count++;
+                    distances[count] = distance(villageRGB[j], cityRGB[i] );
+                    type[count] = 1;
+                    count++;
                 }
-            }
-            averageCity /= 9.0;
-            averageVillage/= 9.0;
 
-            if(averageCity < averageVillage) {
+            for(int k=0; k<18;k++)
+                for(int l=k+1; l<18; l++)
+                    if(distances[k] > distances[l]){
+                        double temp = distances[k];
+                        distances[k] = distances[l];
+                        distances[l] = temp;
+
+                        int tType = type[k];
+                        type[k] = type[l];
+                        type[l] = tType;
+                    }
+
+            int[] knn = new int[2];
+            for(int k=0; k<1;k++)
+                knn[type[k]]++;
+
+            if(knn[0] > knn[1]) {
                 System.out.println("city "+i+" correct");
                 cityCorrect++;
-            }
-            else System.out.println("wrong");
+            } else System.out.println("wrong");
         }
 
         //village testing
         for(int i=0; i<10;i++){
-            double averageCity = 0;
-            double averageVillage = 0;
+            double[] distances = new double[18];
+            int[] type = new int[18];
 
-            for (int j =0; j<10; j++){
+            int count = 0;
+            for (int j =0; j<10; j++)
                 if(i!=j){
-                    averageCity += distance(cityRGB[j], villageRGB[i] );
-                    averageVillage += distance(villageRGB[j],villageRGB[i] );
+                    distances[count] = distance(cityRGB[j], villageRGB[i] );
+                    type[count] = 0;
+                    count++;
+                    distances[count] = distance(villageRGB[j], villageRGB[i] );
+                    type[count] = 1;
+                    count++;
                 }
-            }
-            averageCity /= 9.0;
-            averageVillage/= 9.0;
 
-            if(averageCity > averageVillage) {
+            for(int k=0; k<18;k++)
+                for(int l=k+1; l<18; l++)
+                    if(distances[k] > distances[l]){
+                        double temp = distances[k];
+                        distances[k] = distances[l];
+                        distances[l] = temp;
+
+                        int tType = type[k];
+                        type[k] = type[l];
+                        type[l] = tType;
+                    }
+
+            int[] knn = new int[2];
+            for(int k=0; k<3;k++)
+                knn[type[k]]++;
+
+            if(knn[0] < knn[1]) {
                 System.out.println("village "+i+" correct");
                 villageCorrect++;
-            }
-            else System.out.println("wrong");
+            } else System.out.println("wrong");
         }
 
         System.out.println("city: "+cityCorrect+"\nvillage: "+villageCorrect);
-
     }
 
     private static double distance(int[] a, int[] b){
@@ -80,24 +110,9 @@ public class Driver {
         for (int i=0;i<a.length;i++){
             result+=Math.pow((a[i]-b[i]),2);
         }
-        result = Math.sqrt(result);
-        return result;
+        return Math.sqrt(result);
     }
 
-
-
-
-
-
-
-
-//    private static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight){
-//        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-//        Graphics2D graphics2D = resizedImage.createGraphics();
-//        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
-//        graphics2D.dispose();
-//        return resizedImage;
-//    }
 //    private static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth){
 //        int targetHeight =(targetWidth* originalImage.getHeight())/originalImage.getWidth();
 //        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
